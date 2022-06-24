0f41ca8b60e05d1d2e69146a5909f6e8ee3c6f50
Omole Israel <omolewayeolagd1997@gmail.com>
2022-06-24 17:06:59 +0100

    edits on js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const recipeModule = (() => {
 
-   //PUBLIC NAMESPACING 
 
    const mainSelection = document.querySelector('select[id="main"]');
    const dietSelection = document.querySelector('select[id="diet"]');
@@ -9,13 +8,10 @@ const recipeModule = (() => {
    const resultContainer = document.querySelector('.result-container');
    const ingredients = document.querySelector('.ingredients');
 
-   //PRIVATE NAMESPACING
 
    return {
-      //HANDLE SUBMIT FUNCTION
       handleSubmit: function (e) {
          e.preventDefault();
-         //Get values from all selects
          const mainValue = mainSelection.value;
          const dietValue = dietSelection.value;
          const healthValue = healthSelection.value;
@@ -23,9 +19,7 @@ const recipeModule = (() => {
          this.handleFetch(mainValue, dietValue, healthValue);
       },
 
-      //FETCH ASYNC FUNCTION
-      // 1. validte the response.
-      // - if hits > 0, then process, otherwise, display message no hits
+
       handleFetch: async function (mainValue, dietValue, healthValue) {
          const dataRequest = await fetch(`https://api.edamam.com/search?q=${mainValue}&app_id=08ab47ee&app_key=fa5814a32eb2669676885dff6d983c44&health=${healthValue}&diet=${dietValue}&to=30`);
          const dataResponse = await dataRequest.json()
@@ -36,7 +30,6 @@ const recipeModule = (() => {
       },
 
 
-      //LOOP THROUGH RESULTS
       render: function (data) {
          const html = data.map((item) => {
             return this.renderResult(item);
@@ -45,30 +38,22 @@ const recipeModule = (() => {
          resultContainer.innerHTML = html;
       },
 
-      // return a <li> item
       renderResult: function (data) {
-         //Title 
          const title = data.recipe.label;
-         //Labels
          const label = data.recipe.dietLabels.map(tag => {
             return `<span>${tag}</span>`
          }).join('');
-         //Image
          const image = data.recipe.image;
-         //Tags
          const tags = data.recipe.healthLabels.map(tag => {
             return `<span>${tag}</span>`
          }).join('');
 
-         // Calories 
          const calories = data.recipe.calories;
-         //Ingredients
 
          const ingred = data.recipe.ingredientLines.map(ingredient => {
             return `<li class="ingredient">${ingredient}</li>`;
          }).join('');
 
-         //Link
          const link = data.recipe.url;
 
          return ` 
